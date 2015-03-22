08a00c21dc0cbff4ba4a777036ed0f37b7621117
Gustav Bylund <maistho@gmail.com>
2015-03-22 16:54:09 +0100

    some changes to structure of tweetparser

--- TweetParser.py
+++ TweetParser.py
@@ -13,67 +13,66 @@ class TweetParser:
 
     def __init__(self, hashtag):
         filepath = 'tweets/' + hashtag + '.json'
-        if os.path.isfile(filepath):
-            self.f = open(filepath, 'r')
+        self.f = open(filepath, 'r')
+        self.target_names = ["Hostile", "Nice", "Happy", "Sad", "Angry"]
+        self.tweets = {}
+        self.classifier = None
 
     def getNextTweet(self):
 
         t = self.f.readline()
 
         if t != '':
-            return json.loads(t)
+            tweet = json.loads(t)
+            self.tweets[tweet['id']] = tweet
+            return tweet
         else:
             return None
 
-    def __get_categories(self, target_categories):
+    def add_tags(self, id, tags):
+        self.tweets[id]['tags'] = tags
+
+    def classify_tweets(self, tweets):
+        predicted = self.classifier.predict(tweets)
+        return predicted
+
+    def __get_categories(self):
         categories = []
-        for i, cat in enumerate(target_categories):
+        for i, cat in enumerate(self.target_names):
             print(str(i) + ': ' + cat)
         user_input = input()
         for cat in user_input.split():
-            if int(cat) >= 0 and int(cat) < len(target_categories):
+            if int(cat) >= 0 and int(cat) < len(self.target_names):
                 categories.append(int(cat))
         return categories
 
 
-    def train(self):
+    def train(self, tag_tweets=False):
         trainArr = []
         y_train = []
-        x_test = []
-        tweet = self.getNextTweet()
-
-        target_names = ["Hostile", "Nice", "Happy", "Sad", "Angry"]
-
-        print('Enter the numbers, separated by space, corresponding to the' +
-                'categories you think that the tweet belongs to.\n')
-
-        #User Feedback-loop
-        i = 0
-        while tweet and i < 10:
-            print(tweet['text'] + '\n')
-            y_train.append(self.__get_categories(target_names))
-            trainArr.append(tweet['text'])
-
-            i+=1
+        if tag_tweets:
             tweet = self.getNextTweet()
 
-        i = 0
-        tweet = self.getNextTweet()
-        while i < 5 and tweet:
-            x_test.append(tweet['text'])
+            print('Enter the numbers, separated by space, corresponding to the' +
+                    'categories you think that the tweet belongs to.\n')
 
-            i+=1
-            tweet = self.getNextTweet()
+            #User Feedback-loop
+            i = 0
+            while tweet and i < 10:
+                print(tweet['text'] + '\n')
+                y_train.append(self.__get_categories())
+                trainArr.append(tweet['text'])
+
+                i+=1
+                tweet = self.getNextTweet()
+        else:
+            for tweet in self.tweets.values():
+                if 'tags' in tweet.keys():
+                    trainArr.append(tweet['text'])
+                    y_train.append(tweet['tags'])
 
 
         x_train = np.array(trainArr)
-
-
-        self.model = {
-                'target_names': target_names,
-                'y': y_train,
-                'x': x_train
-                }
 
         classifier = Pipeline([
             ('vectorizer', CountVectorizer(ngram_range=(1,2))),
@@ -82,10 +81,26 @@ class TweetParser:
             ])
 
         classifier.fit(x_train, y_train)
-        predicted = classifier.predict(x_test)
+        self.classifier = classifier
+
+    def test(self):
+        if self.classifier != None:
+            return False
+
+        x_test = []
+        i = 0
+        tweet = self.getNextTweet()
+        while i < 5 and tweet:
+            x_test.append(tweet['text'])
+
+            i+=1
+            tweet = self.getNextTweet()
+
+        predicted = self.classify_tweets(x_test)
 
         for tweet, prediction in zip(x_test, predicted):
-            print('%s => %s' % (tweet, ', '.join(target_names[x] for x in prediction)))
+            print('%s => %s' % (tweet, ', '.join(self.target_names[x] for x in prediction)))
+        return predicted
 
 if __name__ == '__main__':
     # Define some parameters
@@ -95,4 +110,5 @@ if __name__ == '__main__':
         hashtag = sys.argv[1]
 
     t = TweetParser(hashtag)
-    t.train()
+    t.train(tag_tweets=True)
+    t.test()
